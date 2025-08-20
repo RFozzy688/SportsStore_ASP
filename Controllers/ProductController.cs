ca0db8a0d27a68226663c39e69e850b0c11e8ccb
RFozzy688 <pokerfozzy@gmail.com>
2025-08-20 18:07:34 +0300

    glava 9 навигация и корзина

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,29 +7,50 @@ namespace SportsStore.Controllers
     public class ProductController : Controller
     {
         private readonly IProductRepository _repository;
-        public int PageSize = 4;
+        public int PageSize = 2;
 
         public ProductController( IProductRepository repository )
         {
             _repository = repository;
         }
 
-        public ViewResult List(string category, int productPage = 1 ) => View(
-            new ProductsListViewModel
+        public ViewResult List( string category, int productPage = 1 )
+        {
+            var productsListViewModel = new ProductsListViewModel();
+            productsListViewModel.Products = _repository.Products
+                .Where(p => category == null || p.Category == category)
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize);
+
+            productsListViewModel.PagingInfo = new PagingInfo
             {
-                Products = _repository.Products
-                    .Where(p => category == null || p.Category == category)
-                    .OrderBy(p => p.ProductID)
-                    .Skip((productPage - 1) * PageSize)
-                    .Take(PageSize),
-                PagingInfo = new PagingInfo
-                {
-                    CurrentPage = productPage,
-                    ItemsPerPage = PageSize,
-                    TotalItems = _repository.Products.Count()
-                },
-                CurrentCategory = category
-            }
-        );
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = category == null ?
+                    _repository.Products.Count() :
+                    _repository.Products.Where(e => e.Category == category).Count()
+            };
+            productsListViewModel.CurrentCategory = category;
+
+            #region
+            //new ProductsListViewModel
+            //{
+            //    Products = _repository.Products
+            //        .Where(p => category == null || p.Category == category)
+            //        .OrderBy(p => p.ProductID)
+            //        .Skip((productPage - 1) * PageSize)
+            //        .Take(PageSize),
+            //    PagingInfo = new PagingInfo
+            //    {
+            //        CurrentPage = productPage,
+            //        ItemsPerPage = PageSize,
+            //        TotalItems = _repository.Products.Count()
+            //    },
+            //    CurrentCategory = category
+            //}
+            #endregion
+            return View(productsListViewModel);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,23 +11,42 @@ namespace SportsStore
             builder.Services.AddControllersWithViews();
             //builder.Services.AddTransient<IProductRepository, FakeProductRepository>();
             builder.Services.AddTransient<IProductRepository, EFProductRepository>();
+            builder.Services.AddMemoryCache();
+            builder.Services.AddSession();
             builder.Services.AddDbContext<ApplicationDbContext>(option =>
                 option.UseSqlServer(builder.Configuration.GetConnectionString("MsSql")),
                 ServiceLifetime.Singleton);
+
 
             var app = builder.Build();
 
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.MapControllerRoute(
-                name: "pagination",
-                pattern: "Products/Page{productPage}",
-                defaults: new { Controller = "Product", action = "List" }
+                name: null,
+                pattern: "{category}/Page{productPage:int}", 
+                defaults: new { controller = "Product", action = "List" }
+            );
+            app.MapControllerRoute(
+                name: null,
+                pattern: "Page{productPage:int}",
+                defaults: new { controller = "Product", action = "List", productPage = 1}
+            );
+            app.MapControllerRoute(
+                name: null,
+                pattern: "{category}",
+                defaults: new { controller = "Product", action = "List", productPage = 1 }
+            );
+            app.MapControllerRoute(
+                name: null,
+                pattern: "",
+                defaults: new { controller = "Product", action = "List", productPage = 1 }
             );
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Product}/{action=List}/{id?}"
+                pattern: "{controller}/{action}/{id?}"
             );
 
             SeedData.EnsurePopulated(app);
