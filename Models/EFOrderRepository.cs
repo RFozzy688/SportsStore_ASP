3057c2b7dc6576f517d93e52e974460354f72946
RFozzy688 <pokerfozzy@gmail.com>
2025-08-21 18:24:48 +0300

    glava 10 завершение построения корзины для покупок (changed SaveOrder())

--- Models/EFOrderRepository.cs
+++ Models/EFOrderRepository.cs
@@ -15,14 +15,44 @@ namespace SportsStore.Models
             .Include(o => o.Lines)
             .ThenInclude(l => l.Product);
 
+        //public void SaveOrder( Order order )
+        //{
+        //    context.AttachRange(order.Lines.Select(l => l.Product));
+        //    if ( order.OrderID == 0 )
+        //    {
+        //        context.Orders.Add(order);
+        //    }
+        //    context.SaveChanges();
+        //}
+
         public void SaveOrder( Order order )
         {
-            context.AttachRange(order.Lines.Select(l => l.Product));
+            // Убедимся, что продукты в заказе не дублируются в контексте
+            foreach ( var line in order.Lines )
+            {
+                var productId = line.Product.ProductID;
+                var local = context.Products.Local.FirstOrDefault(p => p.ProductID == productId);
+                if ( local != null )
+                {
+                    // Используем локальный экземпляр
+                    line.Product = local;
+                }
+                else
+                {
+                    // Если продукта нет в Local, привязываем его без пометки Modified
+                    context.Products.Attach(line.Product);
+                    // при необходимости можно пометить состояние как Unchanged/Modified
+                    // context.Entry(line.Product).State = EntityState.Unchanged;
+                }
+            }
+
             if ( order.OrderID == 0 )
             {
                 context.Orders.Add(order);
             }
+
             context.SaveChanges();
+
         }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,11 +1,18 @@
-﻿namespace SportsStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Models
 {
     public class Product
     {
         public int ProductID { get; set; }
+        [Required(ErrorMessage = "Please enter а product name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter а description")]
         public string Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter а positive price")]
         public double Price { get; set; }
+        [Required(ErrorMessage = "Please specify а category")]
         public string Category { get; set; }
     }
 }
